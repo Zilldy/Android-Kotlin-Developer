97a4508cd1cfc6455c4c35760897b4a44650ba93
Zilldy <labsfiap@fiap.com.br>
2024-08-15 22:01:07 -0300

    Deletando coisas de outra branch

--- Orgs/app/src/main/java/com/github/zilldy/orgs/model/Produto.kt
+++
@@ -1,9 +0,0 @@
-package com.github.zilldy.orgs.model
-
-import java.math.BigDecimal
-
-class Produto(
-    val nome: String,
-    val descricao: String,
-    val valor: BigDecimal
-)
--- listadecompras/app/src/main/java/zilldy/com/github/listadecompras/model/ItemModel.kt
+++
@@ -1,7 +0,0 @@
-package zilldy.com.github.listadecompras.model
-
-data class ItemModel(
-    val name: String,
-    val onRemove: (ItemModel) -> Unit
-)
-
