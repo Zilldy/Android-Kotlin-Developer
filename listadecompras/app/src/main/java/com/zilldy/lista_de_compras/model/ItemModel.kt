a07328a59c359c49daace5a3d9e6552c07a7e627
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-11 19:07:29 -0300

    Tornando ItemModel uma entidade de banco de dados

--- listadecompras/app/src/main/java/com/zilldy/lista_de_compras/model/ItemModel.kt
+++ listadecompras/app/src/main/java/com/zilldy/lista_de_compras/model/ItemModel.kt
@@ -1,7 +1,11 @@
 package com.zilldy.lista_de_compras.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class ItemModel(
-    val id: Int,
-    val name: String,
-    val onRemove: (ItemModel) -> Unit
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String
 )
